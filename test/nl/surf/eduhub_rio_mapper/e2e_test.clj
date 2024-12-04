9e5191d67d8e972fe32148b99971517102df9002
Michiel de Mare <michieldemare@gmail.com>
2024-12-04 10:58:52 +0100

    Improved e2e tests according to pdf with findings

--- test/nl/surf/eduhub_rio_mapper/e2e_test.clj
+++ test/nl/surf/eduhub_rio_mapper/e2e_test.clj
@@ -17,15 +17,36 @@
 ;; <https://www.gnu.org/licenses/>.
 
 (ns nl.surf.eduhub-rio-mapper.e2e-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.string :as str]
+            [clojure.test :refer :all]
             [nl.jomco.http-status-codes :as http-status]
             [nl.surf.eduhub-rio-mapper.e2e-helper :refer :all]
-            [nl.surf.eduhub-rio-mapper.remote-entities-helper :refer [remote-entities-fixture]])
+            [nl.surf.eduhub-rio-mapper.remote-entities-helper :refer [remote-entities-fixture]]
+            [nl.surf.eduhub-rio-mapper.utils.xml-utils :as xml-utils])
   (:import (java.util UUID)))
 
 (use-fixtures :once with-running-mapper remote-entities-fixture)
 
+(defn kenmerken-tekst-ao [xml naam]
+  (as-> (xml-utils/element->edn xml) $
+        (:Envelope $)
+        (:Body $)
+        (:opvragen_aangebodenOpleiding_response $)
+        (:aangebodenHOOpleiding $)
+        (keep #(:kenmerken %) $)
+        (filter #(= naam (:kenmerknaam %)) $)
+        (first $)
+        (:kenmerkwaardeTekst $)))
+
+
 (deftest ^:e2e create-edspecs-and-program
+  (testing "missing dependent entities"
+    (testing "scenario [4b]: Test /job/upsert with the program. You can expect a new aangeboden opleiding. This aangeboden opleiding includes a periode and a cohort. (you can repeat this to test an update of the same data.)"
+      (let [job (post-job :upsert :programs "some")]
+        (is (job-error? job))
+        (is (str/starts-with? (job-result job :message)
+                              "No 'opleidingseenheid' found in RIO with eigensleutel:")))))
+
   (testing "create edspecs"
     (testing "scenario [1a]: Test /job/dry-run to see the difference between the edspec parent in OOAPI en de opleidingeenheid in RIO. You can expect RIO to be empty, when you start fresh."
       (let [job (post-job :dry-run/upsert :education-specifications "parent-program")]
@@ -33,19 +54,49 @@
           (is (job-done? job))
           (is (job-dry-run-not-found? job)))))
 
-    (testing "scenario [1b]: Test /job/upsert with the program. You can expect 'done' and a opleidingeenheid in RIO is inserted."
+    (testing "scenario [1b]: Test /job/upsert with the education specification. You can expect 'done' and a opleidingeenheid in RIO is inserted."
       (let [parent-job (post-job :upsert :education-specifications "parent-program")
-            parent-code (job-result-opleidingseenheidcode parent-job)]
+            parent-code (job-result-opleidingseenheidcode parent-job)
+            original-rio-sleutel (-> parent-code rio-opleidingseenheid (kenmerken-tekst "eigenOpleidingseenheidSleutel"))]
         (and
           (is (job-done? parent-job))
           (is (job-result-opleidingseenheidcode parent-job))
           (let [xml (rio-opleidingseenheid (job-result-opleidingseenheidcode parent-job))]
+            (is (= "1950-09-20"
+                   (get-in-xml xml ["hoOpleiding" "begindatum"])))
+            (is (= "2060-08-28"
+                   (get-in-xml xml ["hoOpleiding" "einddatum"])))
+            (is (= "HBO-BA"
+                   (get-in-xml xml ["hoOpleiding" "niveau"])))
+            (is (= "1T"
+                   (get-in-xml xml ["hoOpleiding" "hoOpleidingPeriode" "naamKort"])))
             (is (= "parent-program education specification"
-                   (get-in-xml xml ["hoOpleiding" "hoOpleidingPeriode" "naamLang"])))))
+                   (get-in-xml xml ["hoOpleiding" "hoOpleidingPeriode" "naamLang"])))
+            (is (= "93"
+                   (get-in-xml xml ["hoOpleiding" "hoOpleidingPeriode" "studielast"])))
+            (is (= "SBU"
+                   (get-in-xml xml ["hoOpleiding" "hoOpleidingPeriode" "studielasteenheid"])))))
 
         (testing "(you can repeat this to test an update of the same data.)"
           (let [job (post-job :upsert :education-specifications "parent-program")]
-            (is (job-done? job))))
+            (is (job-done? job))
+            (when (job-done? parent-job)
+              (is (job-result-opleidingseenheidcode parent-job))
+              (let [xml (rio-opleidingseenheid (job-result-opleidingseenheidcode parent-job))]
+                (is (= "1950-09-20"
+                       (get-in-xml xml ["hoOpleiding" "begindatum"])))
+                (is (= "2060-08-28"
+                       (get-in-xml xml ["hoOpleiding" "einddatum"])))
+                (is (= "HBO-BA"
+                       (get-in-xml xml ["hoOpleiding" "niveau"])))
+                (is (= "1T"
+                       (get-in-xml xml ["hoOpleiding" "hoOpleidingPeriode" "naamKort"])))
+                (is (= "parent-program education specification"
+                       (get-in-xml xml ["hoOpleiding" "hoOpleidingPeriode" "naamLang"])))
+                (is (= "93"
+                       (get-in-xml xml ["hoOpleiding" "hoOpleidingPeriode" "studielast"])))
+                (is (= "SBU"
+                       (get-in-xml xml ["hoOpleiding" "hoOpleidingPeriode" "studielasteenheid"])))))))
 
         (testing "scenario [1a]: Test /job/dry-run to see the difference between the edspec parent in OOAPI en de opleidingeenheid in RIO. You can expect them to be the same."
           (let [job (post-job :dry-run/upsert :education-specifications "parent-program")]
@@ -55,39 +106,52 @@
               (is (job-without-diffs? job)))))
 
         (testing "scenario [1c]: Test /job/upsert with the edspec child. You can expect 'done' and a variant in RIO is inserted met een relatie met de parent."
-          (let [child-job (post-job :upsert :education-specifications "child-program")
+          (let [test-eigensleutel (UUID/randomUUID)
+                child-job (post-job :upsert :education-specifications "child-program")
                 child-code (job-result-opleidingseenheidcode child-job)]
             (and
               (is (job-done? child-job))
-              (is (rio-with-relation? parent-code child-code))
+              (is (rio-with-relation? (job-result-opleidingseenheidcode parent-job) child-code))
               (let [xml (rio-opleidingseenheid child-code)]
                 (is (= "child-program education specification"
                        (get-in-xml xml ["hoOpleiding" "hoOpleidingPeriode" "naamLang"])))))
 
-            (let [test-eigensleutel (UUID/randomUUID)]
-              (testing "scenario [2a]: Test /job/link of the edspec parent and create a new 'eigen sleutel'. You can expect the 'eigen sleutel' to be changed."
-                (let [job (post-job :link (job-result-opleidingseenheidcode parent-job)
-                                    :education-specifications test-eigensleutel)]
-                  (and
-                    (is (job-done? job))
-                    (is (job-has-diffs? job))))
+            (testing "scenario [2a]: Test /job/link of the edspec parent and create a new 'eigen sleutel'. You can expect the 'eigen sleutel' to be changed."
+              (let [parent-code (job-result-opleidingseenheidcode parent-job)
+                    job         (post-job :link parent-code
+                                          :education-specifications test-eigensleutel)]
+                (and
+                  (is (job-done? job))
+                  (is (= (str test-eigensleutel) (-> (job-result job) :attributes :eigenOpleidingseenheidSleutel :new-id)))
+                  (is (job-has-diffs? job))
+                  (let [rio-sleutel (-> parent-code rio-opleidingseenheid (kenmerken-tekst "eigenOpleidingseenheidSleutel"))]
+                    (is (= (str test-eigensleutel) rio-sleutel)))))
 
-                (testing "(you can repeat this to expect an error because the new 'eigen sleutel' already exists.)"
-                  (let [job (post-job :link (job-result-opleidingseenheidcode child-job)
-                                      :education-specifications test-eigensleutel)]
-                    (is (job-error? job))))))))
+              (testing "(you can repeat this to expect an error because the new 'eigen sleutel' already exists.)"
+                (let [job (post-job :link (job-result-opleidingseenheidcode child-job)
+                                    :education-specifications test-eigensleutel)]
+                  (is (job-error? job)))))))
 
         (testing "scenario [2d]: Test /job/unlink to reset the edspec parent to an empty 'eigen sleutel'."
-          (let [job (post-job :unlink (job-result-opleidingseenheidcode parent-job)
+          (let [parent-code (job-result-opleidingseenheidcode parent-job)
+                job (post-job :unlink parent-code
                               :education-specifications)]
-            (is (job-done? job))))
+            (is (job-done? job))
+            (let [rio-sleutel (-> parent-code rio-opleidingseenheid (kenmerken-tekst "eigenOpleidingseenheidSleutel"))]
+              (is (nil? rio-sleutel)))))
 
         (testing "scenario [2b]: Test /job/link to reset the edspec parent to the old 'eigen sleutel'."
-          (let [job (post-job :link (job-result-opleidingseenheidcode parent-job)
+          (let [parent-code (job-result-opleidingseenheidcode parent-job)
+                job (post-job :link parent-code
                               :education-specifications "parent-program")]
             (and
               (is (job-done? job))
-              (is (job-has-diffs? job))))))))
+              (is (job-has-diffs? job))
+              (is (= (-> (job-result-attributes job) :eigenOpleidingseenheidSleutel :new-id)
+                     original-rio-sleutel))
+              (let [rio-sleutel (-> parent-code rio-opleidingseenheid (kenmerken-tekst "eigenOpleidingseenheidSleutel"))]
+                (is (= original-rio-sleutel
+                       rio-sleutel)))))))))
 
   (testing "create a program (for the edSpec child)"
     (testing "scenario [4a]: Test /job/dry-run to see the difference between the program in OOAPI en de aangeboden opleiding in RIO. You can expect RIO to be empty, when you start fresh."
@@ -127,7 +191,10 @@
                             :programs test-eigensleutel)]
           (and
             (is (job-done? job))
-            (is (job-has-diffs? job))))
+            (is (job-has-diffs? job))
+            (let [ao      (rio-aangebodenopleiding (str (ooapi-id :programs "some")))
+                  sleutel (kenmerken-tekst-ao ao "eigenAangebodenOpleidingSleutel")]
+              (is (= (str test-eigensleutel) sleutel)))))
 
         (testing "(you can repeat this to expect an error because the new 'eigen sleutel' already exists.)"
           (let [job (post-job :link (str (ooapi-id :programs "some"))
@@ -173,7 +240,8 @@
 
   (testing "scenario [7d]: Test /job/upsert with the course. You can expect a new aangeboden opleiding. This aangeboden opleiding includes a periode and a cohort. (you can repeat this to test an update of the same data.)"
     (let [job (post-job :upsert :courses "some")]
-      (is (job-done? job))
+      (and
+        (is (job-done? job)))
       (let [xml (rio-aangebodenopleiding (job-result-aangebodenopleidingcode job))]
         (is (= "1994-09-05"
                (get-in-xml xml ["aangebodenHOOpleidingsonderdeel" "eersteInstroomDatum"])))
@@ -195,7 +263,12 @@
           (is (job-done? job))
           (is (job-has-diffs? job)))))
 
-    (testing "(you can repeat this to expect an error because the new 'eigen sleutel' already exists.)"
+    (testing "check to see if eigensleutel has actually been changed to test-eigensleutel"
+      (let [ao (rio-aangebodenopleiding (str (ooapi-id :courses "some")))
+            sleutel (kenmerken-tekst-ao ao "eigenAangebodenOpleidingSleutel")]
+        (is (= (str test-eigensleutel) sleutel))))
+
+    (testing "(you can repeat the link call and expect no changes as the new 'eigen sleutel' already exists.)"
       (let [job (post-job :link (str (ooapi-id :courses "some"))
                           :courses test-eigensleutel)]
         (and
