925c72ff92a4a465d3149b2c5f295b91a6af2821
Michiel de Mare <merloen@gmail.com>
2025-08-04 10:48:45 +0200

    Update aangeboden_opleiding.clj

--- src/nl/surf/eduhub_rio_mapper/rio/aangeboden_opleiding.clj
+++ src/nl/surf/eduhub_rio_mapper/rio/aangeboden_opleiding.clj
@@ -171,7 +171,7 @@
 
           ;; These are in the xsd but ignored by us
           :eigenAangebodenOpleidingSleutel (some-> id str/lower-case) ;; resolve to the ooapi id
-          :instroomEinddatum (:lastStartDate rio-consumer)
+          :laatsteInstroomdatum (:lastStartDate rio-consumer)
           :opleidingserkenningSleutel nil
           :voVakerkenningSleutel nil)))))
 
